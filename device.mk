4b5704008f1d48a04c9de0af45f4e609d5866e2c
blurzyy <selster02@gmail.com>
2023-01-03 02:59:59 +0800

    Revert "mojito: Update USB HAL to V1.3 implementation"
    This reverts commit a0124df884a62b23f9b63cd4e0a218b629f1a1c1.

--- device.mk
+++ device.mk
@@ -491,7 +491,7 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb@1.0-service
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
@@ -499,6 +499,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
