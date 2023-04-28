e69ef5fd0198a06ff49ac461503ff08b7714b01e
Akash Srivastava <akashniki@gmail.com>
2023-04-28 19:19:04 +0800

    sunny: Update Perf stack from LA.UM.9.14.r1-20200-LAHAINA.QSSI13.0
    Change-Id: I30204b3c46a205a3bc8e74de972bf6f75f94e1b1

--- device.mk
+++ device.mk
@@ -396,14 +396,6 @@ PRODUCT_PACKAGES += \
     SettingsOverlayM2101K7AG \
     SettingsOverlayM2101K7AI
 
-# Perf
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.2.vendor
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
-    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
